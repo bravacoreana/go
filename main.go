666ed79d8f48a796e87321229e835d3526c65d9d
bravacoreana <bravacoreana@gmail.com>
2021-07-11 18:08:16 +0200

    [11/Jul/21] - structs

--- main.go
+++ main.go
@@ -2,22 +2,18 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age int
+	favFood []string
+}
 
 func main() {
-	nico := map[string]string{"name":"nico", "age":"12"}
-	for key, value := range nico {
-		fmt.Println(key , value)
-	}
-	// name nico
-	// age 12
-	for _, value := range nico {
-		fmt.Println(value)
-	}
-	// nico
-	// 12
-	for key, _ := range nico {
-		fmt.Println(key)
-	}
-	// name
-	// age
+	favFood := []string{"kimchi", "ramen"}
+	nico := person {
+		name:"nico",
+		age:18,
+		favFood: favFood,
+        }
+	fmt.Println(nico) //{nico 18 [kimchi ramen]}
 }
